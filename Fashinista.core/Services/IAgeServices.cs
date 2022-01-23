7c8c3f49a750731216e0f94887189d6dce465ba5
haneen <haneenjaradat06@gmail.com>
2022-01-23 13:43:30 +0200

    age services

---
+++ Fashinista.core/Services/IAgeServices.cs
@@ -0,0 +1,23 @@
+﻿using Fashinista.core.Data;
+using Fashinista.core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fashinista.core.Services
+{
+    public interface IAgeServices
+    {
+        public string Insert_Age(Age age);//from service 
+        public bool Delete_Age(int id);
+        public bool Update_Age(Age age);
+
+        public List<Age> GET_All_Age();
+
+
+        public Age Get_Age_By_Id(int id);
+
+
+
+    }
+}
---
+++ Fashinista.infra/Services/AgeServices.cs
@@ -0,0 +1,43 @@
+﻿using Fashinista.core.Data;
+using Fashinista.core.Repository;
+using Fashinista.core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fashinista.infra.Services
+{
+  public  class AgeServices: IAgeServices
+    {
+        private readonly IAgeRepository ageRepository;
+        public AgeServices(IAgeRepository ageRepository)
+        {
+            this.ageRepository = ageRepository;
+        }
+
+        public bool Delete_Age(int id)
+        {
+            return ageRepository.Delete_Age(id);
+        }
+
+        public Age Get_Age_By_Id(int id)
+        {
+            return ageRepository.Get_Age_By_Id(id);
+        }
+
+        public List<Age> GET_All_Age()
+        {
+            return ageRepository.GET_All_Age();
+        }
+
+        public string Insert_Age(Age age)
+        {
+            return ageRepository.Insert_Age(age);
+        }
+
+        public bool Update_Age(Age age)
+        {
+            return ageRepository.Update_Age(age);
+        }
+    }
+}
